9b919634424d9eb8437bee65f9a082fccbbf6b04
sergioruiz28282 <sergioruizort07@gmail.com>
2026-02-12 12:47:53 +0100

    Subida del segundo archivo

---
+++ src/com/ejemplo/p4/Main2.java
@@ -0,0 +1,9 @@
+package com.ejemplo.p4;
+
+public class Main2 {
+	public static void main(String[] args) {
+		System.out.println("Practica 4 - Git casos reales");
+	    System.out.println("Sergio Ruiz Ortiz");
+	}
+
+}
