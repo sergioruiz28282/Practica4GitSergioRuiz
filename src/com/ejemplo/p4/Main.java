765892a8c45017ca7a4400093f0c05b8243411bd
sergioruiz28282 <sergioruizort07@gmail.com>
2026-02-12 12:14:55 +0100

    Inicio proyecto 4

---
+++ src/com/ejemplo/p4/Main.java
@@ -0,0 +1,11 @@
+package com.ejemplo.p4;
+
+public class Main {
+
+	public static void main(String[] args) {
+		System.out.println("Practica 4 - Git casos reales");
+	    System.out.println("Sergio Ruiz Ortiz");
+	}
+
+}
+
