6cbe540af37a925222f2a84dd14980699e0428b9
sergioruiz28282 <sergioruizort07@gmail.com>
2026-02-18 12:10:53 +0100

    Cambio malo subido por error

--- src/com/ejemplo/p4/Main.java
+++ src/com/ejemplo/p4/Main.java
@@ -5,6 +5,7 @@ public class Main {
 	public static void main(String[] args) {
 		System.out.println("Practica 4 - Git casos reales");
 	    System.out.println("Sergio Ruiz Ortiz");
+	    System.out.println("Este es un cambio malo del punto 6");
 	}
 
 }
